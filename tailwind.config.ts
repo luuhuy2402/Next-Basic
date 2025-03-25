ef8a29c84ab7af4658356e951107e3523caf3da0
luuhuy2402 <xeoxeo1895@gmail.com>
2025-03-25 21:59:13 +0700

    Init project

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config = {
+    content: [
+        "./pages/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+        "./app/**/*.{ts,tsx}",
+        "./src/**/*.{ts,tsx}",
+    ],
+    prefix: "",
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+} satisfies Config;
+
+export default config;
